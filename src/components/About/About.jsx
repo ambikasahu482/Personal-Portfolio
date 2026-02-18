2605cce321bab5ad96ed9e0514f28def3b79571e
ambikasahu482 <ambikasahu482@gmail.com>
2026-02-18 18:00:37 +0530

    Add About component

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1j1nC1zoT6gusbs1mKWseB_zij4eApjEk/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1OKgzuu8BRIekYxF_u4sT8pf9KskQysMV/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
