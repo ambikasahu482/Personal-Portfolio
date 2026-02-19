ad2d22406c76249e3d574538ecd16254e91ecb0e
ambikasahu482 <ambikasahu482@gmail.com>
2026-02-19 15:02:39 +0530

    About.jsx file update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1OKgzuu8BRIekYxF_u4sT8pf9KskQysMV/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1ktXr5T-gSfL5pt5sVvEEHXPvHyGyHek1/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
